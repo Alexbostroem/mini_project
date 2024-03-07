92668c05ec4b00d0c303f39819cacad5adbddc31
Alexander Boström <alexander.bostroem@gmail.com>
2024-03-07 09:27:53 +0100

    Börjar med att bygga en fit, predict, score class

---
+++ recovery_predictor.py
@@ -0,0 +1,9 @@
+class recoveryPredictor():
+    def __init__(self, activity, sleep, heart_rate):
+        self.activity = activity
+        self.sleep = sleep
+        self.heart_rate = heart_rate
+
+    def fit(self):
+        
+        
